cfa719bc65a9a71ddd7d74232f234d4057b25108
Lemon <62458905+lemonsama123@users.noreply.github.com>
2022-06-08 22:23:50 +0800

    上传Java核心技术11版第一卷示例代码

--- java-learn/src/Main.java
+++ java-learn/src/Main.java
@@ -1,5 +1,16 @@
+/**
+ * @author
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        Byte b = -12;
+//        System.out.println(b);
+//        System.out.println(Byte.toUnsignedInt(b));
+//        System.out.println(0x1.0p-3);
+//        System.out.println(2.0 - 1.1);
+        for (int i = 0; i < 0xffff; i++) {
+
+        }
+        System.out.println("\u03c0");
     }
 }
--- java-learn/src/corejava/v1ch03/CompoundInterest/CompoundInterest.java
+++ java-learn/src/corejava/v1ch03/CompoundInterest/CompoundInterest.java
@@ -2,13 +2,12 @@ package corejava.v1ch03.CompoundInterest;
 
 /**
  * This program shows how to store tabular data in a 2D array.
- * @version 1.40 2004-02-10
+ *
  * @author Cay Horstmann
+ * @version 1.40 2004-02-10
  */
-public class CompoundInterest
-{
-   public static void main(String[] args)
-   {
+public class CompoundInterest {
+   public static void main(String[] args) {
       final double STARTRATE = 10;
       final int NRATES = 6;
       final int NYEARS = 10;
@@ -25,10 +24,8 @@ public class CompoundInterest
          balances[0][j] = 10000;
 
       // compute interest for future years
-      for (int i = 1; i < balances.length; i++)
-      {
-         for (int j = 0; j < balances[i].length; j++)
-         {
+      for (int i = 1; i < balances.length; i++) {
+         for (int j = 0; j < balances[i].length; j++) {
             // get last year's balances from previous row
             double oldBalance = balances[i - 1][j];
 
@@ -47,8 +44,7 @@ public class CompoundInterest
       System.out.println();
 
       // print balance table
-      for (double[] row : balances)
-      {
+      for (double[] row : balances) {
          // print table row
          for (double b : row)
             System.out.printf("%10.2f", b);
