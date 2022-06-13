b1bc563f610c87e855780ba49c96e557e1037526
Lemon <62458905+lemonsama123@users.noreply.github.com>
2022-06-13 20:56:19 +0800

    updata all

--- java-learn/src/Main.java
+++ java-learn/src/Main.java
@@ -3,14 +3,9 @@
  */
 public class Main {
     public static void main(String[] args) {
-//        Byte b = -12;
-//        System.out.println(b);
-//        System.out.println(Byte.toUnsignedInt(b));
-//        System.out.println(0x1.0p-3);
-//        System.out.println(2.0 - 1.1);
-        for (int i = 0; i < 0xffff; i++) {
-
-        }
-        System.out.println("\u03c0");
+        int x = 0xffff_ff9f;
+        System.out.println(Integer.toHexString(x));
+        System.out.println(Integer.toHexString(x >> 2));
+        System.out.println(Integer.toHexString(x >>> 2));
     }
 }
