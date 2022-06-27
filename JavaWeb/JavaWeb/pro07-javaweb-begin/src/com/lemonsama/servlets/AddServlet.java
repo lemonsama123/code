9f1d295bb62de756ea4ac2d724ab0b5765f7f70b
Lemon <62458905+lemonsama123@users.noreply.github.com>
2022-06-27 10:57:29 +0800

    updata all

--- JavaWeb/JavaWeb/pro07-javaweb-begin/src/com/lemonsama/servlets/AddServlet.java
+++ JavaWeb/JavaWeb/pro07-javaweb-begin/src/com/lemonsama/servlets/AddServlet.java
@@ -1,5 +1,9 @@
 package com.lemonsama.servlets;
 
+import com.lemonsama.fruit.dao.FruitDAO;
+import com.lemonsama.fruit.dao.impl.FruitDAOImpl;
+import com.lemonsama.fruit.pojo.Fruit;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,12 +17,17 @@ public class AddServlet extends HttpServlet {
         String fname = req.getParameter("fname");
         String priceStr = req.getParameter("price");
         Integer price = Integer.parseInt(priceStr);
-        String fount = req.getParameter("fcount");
+        String fcountStr = req.getParameter("fcount");
+        Integer fcount = Integer.parseInt(fcountStr);
         String remark = req.getParameter("remark");
 
-        System.out.println("fname = " + fname);
-        System.out.println("price = " + price);
-        System.out.println("fount = " + fount);
-        System.out.println("remark = " + remark);
+        FruitDAO fruitDAO = new FruitDAOImpl();
+        boolean flag = fruitDAO.addFruit(new Fruit(0, fname, price, fcount, remark));
+        System.out.println(flag ? "添加成功！" : "添加失败！");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
     }
 }
