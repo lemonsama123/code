65b939b7b4afd3edd7796b3377e436a80a10bdae
Lemon <62458905+lemonsama123@users.noreply.github.com>
2022-06-28 19:48:04 +0800

    updata all

---
+++ spring5_demo6/src/com/lemon/spring5/config/TxConfig.java
@@ -0,0 +1,42 @@
+package com.lemon.spring5.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.lemon")
+@EnableTransactionManagement
+public class TxConfig {
+
+    @Bean
+    public DruidDataSource getDruidDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql:///user_db");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public DataSourceTransactionManager getDataSourceTransactionManager(DataSource dataSource) {
+        DataSourceTransactionManager dataSourceTransactionManager =
+                new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource);
+        return dataSourceTransactionManager;
+    }
+}
--- spring5_demo6/src/com/lemon/spring5/test/TestTX.java
+++ spring5_demo6/src/com/lemon/spring5/test/TestTX.java
@@ -1,17 +1,37 @@
 package com.lemon.spring5.test;
 
+import com.lemon.spring5.config.TxConfig;
 import com.lemon.spring5.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestTX {
 
     @Test
-    public void testAccount() {
+    public void testAccount1() {
 
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("bean1.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.accountMoney();
+    }
+
+    @Test
+    public void testAccount2() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean2.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.accountMoney();
+    }
+
+    @Test
+    public void testAccount3() {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(TxConfig.class);
         UserService userService = context.getBean("userService", UserService.class);
         userService.accountMoney();
     }
