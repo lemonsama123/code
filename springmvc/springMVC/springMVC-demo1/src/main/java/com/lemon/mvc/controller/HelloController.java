e813c736a0b65d30f4e93dff0046cf30e05a846b
Lemon <62458905+lemonsama123@users.noreply.github.com>
2022-06-30 18:07:04 +0800

    updata all

---
+++ springmvc/springMVC/springMVC-demo1/src/main/java/com/lemon/mvc/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.lemon.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/target")
+    public String toTar() {
+        return "target";
+    }
+}
