01b98292c209c7fa0ca7bcfc2a190369ffe737a0
Lemon <62458905+lemonsama123@users.noreply.github.com>
2022-07-02 16:23:52 +0800

    updata all

---
+++ springmvc/springMVC/springMVC-demo4/src/main/java/com/lemon/mvc/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.lemon.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Classname TestController
+ * @Description TODO
+ * @Date 2022/7/2 15:43
+ * @Created yj
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/testInterceptor")
+    public String testInterceptor() {
+        return "success";
+    }
+}
