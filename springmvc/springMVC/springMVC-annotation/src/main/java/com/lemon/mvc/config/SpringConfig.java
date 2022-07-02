661e1db8634adc1dcca0c391df9da6764e91beb5
Lemon <62458905+lemonsama123@users.noreply.github.com>
2022-07-02 18:25:25 +0800

    updata all

---
+++ springmvc/springMVC/springMVC-annotation/src/main/java/com/lemon/mvc/config/SpringConfig.java
@@ -0,0 +1,13 @@
+package com.lemon.mvc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Classname SpringConfig
+ * @Description TODO
+ * @Date 2022/7/2 18:02
+ * @Created yj
+ */
+@Configuration
+public class SpringConfig {
+}
---
+++ springmvc/springMVC/springMVC-annotation/src/main/java/com/lemon/mvc/config/WebConfig.java
@@ -0,0 +1,57 @@
+package com.lemon.mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+/**
+ * @Classname WebConfig
+ * @Description TODO
+ * @Date 2022/7/2 18:03
+ * @Created yj
+ */
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebConfig {
+
+    //配置生成模板解析器
+    @Bean
+    public ITemplateResolver templateResolver() {
+        WebApplicationContext webApplicationContext = ContextLoader.getCurrentWebApplicationContext();
+        // ServletContextTemplateResolver需要一个ServletContext作为构造参数，可通过WebApplicationContext 的方法获得
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(
+                webApplicationContext.getServletContext());
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+
+    //生成模板引擎并为模板引擎注入模板解析器
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    //生成视图解析器并未解析器注入模板引擎
+    @Bean
+    public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setCharacterEncoding("UTF-8");
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
+}
