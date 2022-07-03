d4329e14ade08609d1a482988c8e33472bd5b93f
Lemon <62458905+lemonsama123@users.noreply.github.com>
2022-07-03 10:16:48 +0800

    updata all

---
+++ MyBatis/MyBatis/MyBitis_demo1/src/main/java/com/lemon/mybitis/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.lemon.mybitis.mapper;
+
+/**
+ * @Classname UserMapper
+ * @Description TODO
+ * @Date 2022/7/3 10:09
+ * @Created yj
+ */
+public interface UserMapper {
+
+    int insertUser();
+
+}
---
+++ MyBatis/MyBatis/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;/**
+ * @Classname ${NAME}
+ * @Description TODO
+ * @Date ${DATE} ${TIME}
+ * @Created yj
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
